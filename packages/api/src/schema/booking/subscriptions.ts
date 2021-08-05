7fa62429510660804906bba0d4a5bcc4bbf4d8da
ntoulemont <n.toulemont@gmail.com>
2021-08-05 19:43:19 +0200

    booking subscriptions done

--- packages/api/src/schema/booking/subscriptions.ts
+++ packages/api/src/schema/booking/subscriptions.ts
@@ -1,70 +1,89 @@
-import { idArg, list, nonNull, subscriptionField } from 'nexus'
-import { authorizeUser } from '@utils/authorization'
+import { idArg, list, nonNull, subscriptionField, unionType } from 'nexus'
+import { authorize } from '../../utils'
 import { withFilter } from 'apollo-server-express'
 import { CREATED_BOOKING, NEW_MESSAGE, UPDATED_BOOKING } from '../../constants/pubsub'
 
+export const BookingMessageSubscriptionResult = unionType({
+	name: 'BookingMessageSubscriptionResult',
+	definition(t) {
+		t.members(
+			'UserBookingMessage',
+			'OperatorBookingMessage',
+			'UserAuthenticationError',
+			'UserForbiddenError'
+		)
+	}
+})
+
 export const bookingByIdChatSub = subscriptionField('bookingByIdChatSub', {
-	type: 'MessageResponse',
+	type: 'BookingMessageSubscriptionResult',
 	args: {
-		bookingId: nonNull(idArg()),
+		bookingId: nonNull(idArg())
 	},
 	subscribe: withFilter(
 		(_, __, ctx) => ctx.pubsub.asyncIterator(NEW_MESSAGE),
-		(payload, variables) => payload.bookingByIdChatSub.bookingId === variables.bookingId,
+		(payload, variables) => payload.bookingByIdChatSub.bookingId === variables.bookingId
 	),
-	authorization: (ctx) => authorizeUser(ctx, 'user'),
+	authorization: (ctx) => authorize(ctx, 'user'),
 	resolve(root) {
 		return { message: root.bookingByIdChatSub }
-	},
+	}
 })
 
 export const userBookingsMessagesChangesSub = subscriptionField('userBookingsMessagesChangesSub', {
-	type: 'MessageResponse',
+	type: 'BookingMessageSubscriptionResult',
 	args: {
 		bookingIds: list(idArg()),
-		authorId: idArg(),
+		authorId: idArg()
 	},
 	subscribe: withFilter(
 		(_, __, ctx) => ctx.pubsub.asyncIterator(NEW_MESSAGE),
 		(payload, variables) =>
 			variables.bookingIds.includes(payload.bookingByIdChatSub.bookingId) &&
-			payload.bookingByIdChatSub.authorId !== variables.authorId,
+			payload.bookingByIdChatSub.authorId !== variables.authorId
 	),
-	authorization: (ctx) => authorizeUser(ctx, 'user'),
+	authorization: (ctx) => authorize(ctx, 'user'),
 	resolve(root) {
 		return { message: root.bookingByIdChatSub }
-	},
+	}
+})
+
+export const BookingSubscriptionResult = unionType({
+	name: 'BookingSubscriptionResult',
+	definition(t) {
+		t.members('Booking', 'UserAuthenticationError', 'UserForbiddenError')
+	}
 })
 
 export const userBookingsStatusChangesSub = subscriptionField('userBookingsStatusChangesSub', {
-	type: 'BookingResponse',
+	type: 'BookingSubscriptionResult',
 	args: {
-		bookingIds: list(idArg()),
+		bookingIds: list(idArg())
 	},
 	subscribe: withFilter(
 		(_, __, ctx) => ctx.pubsub.asyncIterator(UPDATED_BOOKING),
-		(payload, variables) => variables.bookingIds.includes(payload.booking.id),
+		(payload, variables) => variables.bookingIds.includes(payload.booking.id)
 	),
-	authorization: (ctx) => authorizeUser(ctx, 'user'),
+	authorization: (ctx) => authorize(ctx, 'user'),
 	resolve({ booking }) {
 		return { booking }
-	},
+	}
 })
 
 export const newlyCreatedBookingsAsSitterSub = subscriptionField(
 	'newlyCreatedBookingsAsSitterSub',
 	{
-		type: 'BookingResponse',
+		type: 'BookingSubscriptionResult',
 		args: {
-			sitterId: idArg(),
+			sitterId: idArg()
 		},
 		subscribe: withFilter(
 			(_, __, ctx) => ctx.pubsub.asyncIterator(CREATED_BOOKING),
-			(payload, variables) => variables.sitterId === payload.booking.sitterId,
+			(payload, variables) => variables.sitterId === payload.booking.sitterId
 		),
-		authorization: (ctx) => authorizeUser(ctx, 'user'),
+		authorization: (ctx) => authorize(ctx, 'user'),
 		resolve({ booking }) {
 			return { booking }
-		},
-	},
+		}
+	}
 )
